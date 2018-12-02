029983044b4a2ff9febc32aaaf2548e52643cd88
neoliang <781991051@qq.com>
2018-12-02 21:14:36 +0800

    创建：液态虚拟摇杆效果
    upload liquid joystick

---
+++ Assets/liquidJoystick/scripts/inputManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputManager : MonoBehaviour {
+
+    public liquidJoystick joystick = null;
+
+    Vector2 startPosition = Vector2.zero;
+
+    //scale
+    float hideScale = 0f;
+    float showScale = 1f;
+    bool isToShow = false;
+    float scaleSpeed = 3.2f;
+
+    //shape change rate
+    float changeDistanceReference = Screen.width * 0.2f;
+    float changedRate = 0f;
+    bool shouldReturnChange = false;
+    float changeReturnSpeed = 40f;
+
+    float angle = 0;
+
+    //test cube
+    public GameObject cube;
+    Vector2 cubeLeftBottom = new Vector2(-50f,0f);
+    Vector2 cubeRightTop = new Vector2(50f, 40f);
+    float cubeMoveSpeed = 40f;
+
+    void Update () {
+        WhenTouched();
+        ChangeScale();
+        if (shouldReturnChange)
+        {
+            ShapeChangeReturn();
+        }
+    }
+
+    void WhenTouched()
+    {
+        //Touch touch = Input.GetTouch(0);
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            isToShow = true;
+            shouldReturnChange = false;
+            joystick.SetLiquidJoystick(startPosition, 0f, 0f);
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            shouldReturnChange = true;
+            isToShow = false;
+        }else if (Input.GetMouseButton(0))
+        {
+            Vector2 diff = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - startPosition;
+            float distance = Mathf.Pow(diff.x * diff.x + diff.y * diff.y, 0.5f);
+            angle = 0;
+            changedRate = 0;
+            float minDistance = 1f;
+            if (distance > minDistance)
+            {
+                changedRate = distance / changeDistanceReference;
+                angle = 180f - Vector2.SignedAngle(diff, Vector2.right);
+                MoveCube(diff / distance);
+            }
+            joystick.SetLiquidJoystick(startPosition, angle, changedRate);
+            
+        }
+    }
+
+    void ChangeScale()
+    {
+        float scaleStep = scaleSpeed * Time.deltaTime;
+        float nextScale;
+        RectTransform rectTran = GetComponent<RectTransform>();
+        if (isToShow)
+        {
+            nextScale = rectTran.localScale.x + scaleStep;
+            if(nextScale > showScale)
+            {
+                nextScale = showScale;
+            }
+            rectTran.localScale = new Vector3(nextScale, nextScale, 1f);
+        }
+        else
+        {
+            nextScale = rectTran.localScale.x - scaleStep;
+            if (nextScale < hideScale)
+            {
+                nextScale = hideScale;
+            }
+            rectTran.localScale = new Vector3(nextScale, nextScale, 1f);
+        }
+    }
+
+    void ShapeChangeReturn()
+    {
+        float nextRate = changedRate - Time.deltaTime * changeReturnSpeed;
+        if(nextRate < 0f)
+        {
+            nextRate = 0f;
+        }
+        joystick.SetLiquidJoystick(startPosition, angle, nextRate);
+    }
+
+    void MoveCube(Vector2 direction)
+    {
+        Vector3 position = cube.transform.position;
+        float x = position.x + direction.x * cubeMoveSpeed * Time.deltaTime;
+        x = (x < cubeLeftBottom.x) ? cubeLeftBottom.x : x;
+        x = (x > cubeRightTop.x) ? cubeRightTop.x : x;
+
+        float y = position.y + direction.y * cubeMoveSpeed * Time.deltaTime;
+        y = (y < cubeLeftBottom.y) ? cubeLeftBottom.y : y;
+        y = (y > cubeRightTop.y) ? cubeRightTop.y : y;
+
+        cube.transform.position = new Vector3(x, y, position.z);
+    }
+}
---
+++ Assets/liquidJoystick/scripts/liquidJoystick.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class liquidJoystick : Graphic {
+
+    public Mesh normalMesh = null;
+    public Mesh changedMesh = null;
+
+    double changeRate = 0f;
+
+    [Range(0.01f, 1f)]
+    public float sizeRate = 0.5f;
+    float scaleSize = 40;
+
+    protected override void Start()
+    {
+        float minSizeReferrence = (Screen.width < Screen.height) ? Screen.width : Screen.height;
+        scaleSize = sizeRate * minSizeReferrence;
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+        List<Vector3> normalVectors = new List<Vector3>();
+        normalMesh.GetVertices(normalVectors);
+        List<Vector2> normalUVs = new List<Vector2>();
+        normalMesh.GetUVs(0,normalUVs);
+
+        List<Vector3> changedVectors = new List<Vector3>();
+        changedMesh.GetVertices(changedVectors);
+
+        Vector3 position = Vector3.zero;
+        double x, y;
+        for(int i = 0;i < normalVectors.Count; i++)
+        {
+            x = normalVectors[i].x * (1.0 - changeRate) + changedVectors[i].x * changeRate;
+            y = normalVectors[i].z * (1.0 - changeRate) + changedVectors[i].z * changeRate;
+            position = new Vector3( (float)x, (float)y ,0);
+            vh.AddVert(position * scaleSize, color, normalUVs[i]);
+        }
+        
+        for (int i = 2;i < normalMesh.triangles.Length; i += 3)
+        {
+            vh.AddTriangle(normalMesh.triangles[i-2], normalMesh.triangles[i - 1], normalMesh.triangles[i]);
+        }
+    }
+
+    /// <summary>
+    /// 查阅UGUI Image实现源码，发现添加mainTexture设置才能让自己添加的texture生效
+    /// </summary>
+    public override Texture mainTexture
+    {
+        get
+        {
+            if (material != null && material.mainTexture != null)
+            {
+                return material.mainTexture;
+            }
+            return s_WhiteTexture;
+        }
+    }
+
+    /// <summary>
+    /// 外部唯一需要调用的方法
+    /// </summary>
+    /// <param name="position">摇杆所在中心位置</param>
+    /// <param name="angle">摇杆旋转角度</param>
+    /// <param name="rate">摇杆变形率</param>
+    public void SetLiquidJoystick(Vector2 position, float angle, float rate)
+    {
+        GetComponent<RectTransform>().position = position;
+        transform.eulerAngles = new Vector3(0f,0f,angle);
+
+        rate = (rate > 1f) ? 1f : rate;
+        rate = (rate < 0f) ? 0f : rate;
+        changeRate = rate;
+        SetVerticesDirty();
+    }
+}
